b361121c6614bde93e7432f387f89337cfd14d1a
Jason Carey <jcarey@cheetahmail.com>
2011-01-24 13:51:37 -0500

    redirect tail 2>/dev/null

--- Term/App/Event/TailFile.pm
+++ Term/App/Event/TailFile.pm
@@ -13,7 +13,7 @@ has 'filename' => (is => 'ro', isa => 'Str', required => 1);
 sub _build_command {
   my $self = shift;
 
-  "tail -n +0 -F " . $self->filename;
+  "tail -n +0 -F " . $self->filename . " 2>/dev/null";
 }
 
 no Moose;
